527ac7bd5fc9bc1c935f8e4d0022760277e95db7
y-sano <mmyy125115@gmail.com>
2026-05-09 20:56:11 +0900

    clarityの導入方法変更

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,7 @@
 <script lang="ts">
-	import ClarityScript from '$lib/presentation/shared/analytics/ClarityScript.svelte';
 	import '../app.css';
 
 	let { children } = $props();
 </script>
-
-<ClarityScript />
 
 {@render children()}
